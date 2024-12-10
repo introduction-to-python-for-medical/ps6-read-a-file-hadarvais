cfd71c6378229b2c99b2ea6f1cf46265111e6383
hadarvais <34959680+hadarvais@users.noreply.github.com>
2024-12-10 17:08:14 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    file = open(file_path)
+    rows = file.readlines()
+    file.close()
+    new_dict = {}
+    for row in rows:
+      row = row.strip()
+      row = row.split('\t')
+      key = row[0]
+      value = row[2]
+      new_dict[key] = value
+    return new_dict
 
 
