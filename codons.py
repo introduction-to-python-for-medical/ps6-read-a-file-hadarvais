12c997566b8a08a37a910a2ebe888d1f634e1316
hadarvais <34959680+hadarvais@users.noreply.github.com>
2025-02-23 18:49:50 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,14 +1,14 @@
 def create_codon_dict(file_path):
-    file = open(file_path)
-    rows = file.readlines()
-    file.close()
-    new_dict = {}
-    for row in rows:
-      row = row.strip()
-      row = row.split('\t')
-      key = row[0]
-      value = row[2]
-      new_dict[key] = value
-    return new_dict
+    file = open(file_path) #נפתח את הקובץ שלנו לתוך משתנה חדש
+    rows = file.readlines() #נקרא את כל השורות בקוכץ ונכניס אותן לתוך מחרוזת חדשה
+    file.close() #נסגור את הקובץ
+    new_dict = {} #נגדיר מילון חדש
+    for row in rows: #עבור כל שורה ברשימת השורות
+      row = row.strip() #ננקה את השורה
+      row = row.split('\t') #נפצל את השורה במקומות הרלוונטיים
+      key = row[0] #המפתח של המילון מוחזק בשורה במיקום ה0
+      value = row[2]  #הערך מוחזק בשורה במיקום השני
+      new_dict[key] = value #נוסיף לרשימה שהגדרנו צמדים של מפתח והערך שתואם לו
+    return new_dict #נחזיר את המילון החדש
 
 
